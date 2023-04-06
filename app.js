853bdc2574260df9a3d27ab7b7e95b5bf5fd2042
oscarmendozaa <mendoza.oscar0827@gmail.com>
2023-04-06 12:03:10 -0600

    Adding final transitions and accurate text

--- app.js
+++ app.js
@@ -3,3 +3,19 @@ const btn = document.getElementById('btn-submit');
 btn.addEventListener('click', function() {
   alert('Tu mensaje ha sido enviado.');
 });
+
+//Loading animation
+const loadingAnimation = document.querySelector('.loading-animation');
+
+loadingAnimation.style.display = 'none';
+
+const navbarTabs = document.querySelectorAll('.navbar-tab');
+navbarTabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    loadingAnimation.style.display = 'flex';
+
+    setTimeout(() => {
+      loadingAnimation.style.display = 'none';
+    }, 3000); 
+  });
+});
